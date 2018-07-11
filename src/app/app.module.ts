3047d8f809c13ba59d30188dbdeaf230b67947c8
kuangyazhou <kuangyazhou@tuandai.com>
2018-07-11 10:54:07 +0800

    search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 
 // import { AppComponent } from './app.component';
 import { HeroComponent } from "../hero/hero.component";
 import { DetailComponent } from "../detail/detail.component";
-import { MessageComponent } from './message/message.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { MessageComponent } from "./message/message.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+
+import InMemoryDataService from "./in-memory-data.service";
 
 @NgModule({
-  declarations: [HeroComponent, DetailComponent, MessageComponent, DashboardComponent],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [
+    HeroComponent,
+    DetailComponent,
+    MessageComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule
+  ],
   providers: [],
   bootstrap: [HeroComponent]
 })
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
+// import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 
 import Hero from "../hero/hero";
 import HEROS from "../hero/mockHeros";
@@ -10,13 +12,43 @@ import { MessageService } from "./message.service";
   providedIn: "root"
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  constructor(
+    // private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
+  private heroesUrl = "api/heroes"; // URL to web api
   // getHeroes(): Hero[] {
   //   return HEROS;
   // }
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
   getHeroes(): Observable<Hero[]> {
     this.messageService.add("HeroService: fetched heroes");
     return of(HEROS);
+    // return this.http.get<Hero[]>(this.heroesUrl).pipe(
+    //   catchError(this.handleError('getHeroes',[]))
+    // );
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (term.trim()) {
+      return of([]);
+    }
+
+    // return this.http.get<Hero[]>(`api/heroes/?name=${term}`).pipe(
+    //   tap(_ => console.log(`found heroes matching "${term}"`)),
+    //   catchError(this.handleError<Hero[]>("searchHeroes", []))
+    // );
   }
 }
